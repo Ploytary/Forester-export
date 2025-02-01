08ff3d488e5ebfd83785ff45ba53a53a1549c665
Ploytary <126991131+Ploytary@users.noreply.github.com>
2025-02-01 22:33:46 +0300

    to gh pages

--- src/assets/scripts/js/pages/all/initLayoutDebugger.ts
+++
@@ -1,5 +0,0 @@
-import { LayoutDebugger } from '../../libs/LayoutDebugger/LayoutDebugger';
-
-export function initLayoutDebugger() {
-  window.addEventListener('load', () => new LayoutDebugger());
-}
